69b9378c9f58d796ec93895b8263f9ac3f1b1b4b
talesmm14 <talesmelquiades@hotmail.com>
2022-08-23 15:17:54 -0300

    adição do cmakelist e atualização das funções da lib

---
+++ include/NBioAPI_CheckValidity.h
@@ -0,0 +1,14 @@
+#ifndef _NBioAPI_CHECK_SIGNED_DLL_H
+#define _NBioAPI_CHECK_SIGNED_DLL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+NBioAPI_RETURN NBioAPI_CheckValidity(LPCTSTR szModulePath);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif//_NBioAPI_CHECK_SIGNED_DLL_H
